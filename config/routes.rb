db3870647ece9b09232e2a7f96f0c0afdcf4b789
root <root@localhost>
2021-11-11 22:19:27 +0000

    navigation changes

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   resources :receipts
   resources :expense_types
 
-  root 'receipts#index'
+  root 'receipts#new'
   devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
   devise_scope :admin do
     get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
